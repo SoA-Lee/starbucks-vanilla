fc425e63ed00471504eeae14afff763652422bec
SoA-Lee <llsa0927@naver.com>
2021-12-24 12:36:25 +0900

    [mod] All_notice

--- js/main.js
+++ js/main.js
@@ -51,7 +51,7 @@ const fadeEls = document.querySelectorAll('.visual .fade-in');
 // fadeEls 갯수만큼 함수로 실행이 된다.
 fadeEls.forEach(function (fadeEl, index) {
   gsap.to(fadeEl, 1, {
-    delay: (index+1)*.7, //0.7, 1.4, 2.1, 2.7
+    delay: (index+1)*.7, // 0.7, 1.4, 2.1, 2.7
     opacity: 1
   });
 });
