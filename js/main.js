43177b901e44bf7405db518db51994dc1c92416f
SoA-Lee <llsa0927@naver.com>
2021-12-27 14:47:55 +0900

    [add] promotion_pagination

--- js/main.js
+++ js/main.js
@@ -63,3 +63,22 @@ new Swiper('.notice-line .swiper-container',{
   autoplay: true,
   loop: true
 });
+
+new Swiper('.promotion .swiper-container',{
+  // direction: 'horizontal',
+  slidesPerView: 3, // 한번에 보여줄 슬라이드 개수
+  spaceBetween: 10, // 슬라이드 사이 여백
+  centeredSlides: true, // 1번 슬라이드가 가운데 보이기
+  autoplay: {
+    delay: 5000 // 0.5초에 한번 씩 됨. 기본 값 3000
+  },
+  loop: true,
+  pagination: {
+    el: '.promotion .swiper-pagination', // 페이지 번호 요소 선택자
+    clickable: true // 사용자의 페이지 번호 요소 제어 여부
+  },
+  navigation: {
+    prevEl: '.promotion .swiper-prev',
+    nextEl: '.promotion .swiper-next'
+  }
+});
